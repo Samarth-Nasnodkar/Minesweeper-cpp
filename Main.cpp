823af467b8f6b889b4d674e211b89e53ecd7a4a1
Samarth Nasnodkar <70008729+Cooldude069@users.noreply.github.com>
2021-05-29 20:10:22 +0530

    Added Chrono to time the execution

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
+#include<chrono>
 #include"Minesweeper.hpp"
 
 int main(){
     Minesweeper ms;
+    auto start = std::chrono::high_resolution_clock::now();
     ms.generate();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     ms.display();
+    std::cout<<"Executed in "<<duration.count()<<" micros\n";
     return 0;
 }
